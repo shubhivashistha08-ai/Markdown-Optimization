d9a41f6a7f8e43a038c59523f39f3e216836cb88
shubhivashistha08-ai <shubhivashistha08@gmail.com>
2025-12-26 17:33:08 +0530

    Update data_loading.py

--- src/data_loading.py
+++ src/data_loading.py
@@ -2,5 +2,5 @@ from pathlib import Path
 import pandas as pd
 
 def load_markdown_data() -> pd.DataFrame:
-    csv_path = Path(__file__).parent / "SYNTHETIC Markdown Dataset.csv"
+    csv_path = Path(__file__).parent / "synthetic_markdown_dataset.csv"
     return pd.read_csv(csv_path)
